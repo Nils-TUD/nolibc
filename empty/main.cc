0375f2127ca4dc531fbf26b3a3ce3c8ccdf9ce91
Nils Asmussen <nils@os.inf.tu-dresden.de>
2020-10-14 10:54:04 +0200

    Proper exit and getpid in empty.

--- empty/main.cc
+++ empty/main.cc
@@ -1,1 +1,36 @@
-/* write a program */
+#include <syscall.h>
+
+static unsigned syscall0(unsigned no) {
+    asm volatile (
+        "int $0x80"
+        : "+a"(no)
+    );
+    return no;
+}
+
+static unsigned syscall1(unsigned no, unsigned arg1) {
+    asm volatile (
+        "int $0x80"
+        : "+a"(no)
+        : "b"(arg1)
+    );
+    return no;
+}
+
+static unsigned getpid() {
+    return syscall0(__NR_getpid);
+}
+
+static void exit(int result) {
+    syscall1(__NR_exit, result);
+}
+
+int main() {
+	return getpid();
+}
+
+extern "C" void _start() {
+    int result = main();
+    exit(result);
+    __builtin_unreachable();
+}
