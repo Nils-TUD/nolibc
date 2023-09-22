06ca8bb934929583eb1d69121a36877e69fb89cb
Nils Asmussen <nils@os.inf.tu-dresden.de>
2023-09-22 11:43:06 +0200

    Proper startup for empty.

--- empty/main.cc
+++ empty/main.cc
@@ -1,10 +1,16 @@
+#include <sys/syscall.h>
+
 /* write a program */
 int main() {
     return 0;
 }
 
-extern "C" void _start() {
-    main();
-    while(1)
-        ;
+static int syscall1(int no, int arg1) {
+    int v = no;
+    asm volatile (
+        "int $0x80"
+        : "+a"(v)
+        : "b"(arg1)
+    );
+    return v;
 }
