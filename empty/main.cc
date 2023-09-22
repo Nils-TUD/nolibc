038237c62de0c7d8544ba4d7ec5be886f832bd2b
Nils Asmussen <nils@os.inf.tu-dresden.de>
2023-09-22 14:18:55 +0200

    Added wordcount.

--- empty/main.cc
+++ empty/main.cc
@@ -1,16 +1,50 @@
 #include <sys/syscall.h>
 
-/* write a program */
-int main() {
-    return 0;
-}
-
-static int syscall1(int no, int arg1) {
+static int syscall3(int no, unsigned arg1, unsigned arg2, unsigned arg3) {
     int v = no;
     asm volatile (
         "int $0x80"
         : "+a"(v)
-        : "b"(arg1)
+        : "b"(arg1), "c"(arg2), "d"(arg3)
     );
     return v;
+}
+
+unsigned strlen(const char *s) {
+    unsigned n = 0;
+    while(*s++)
+        n++;
+    return n;
+}
+
+void puts(const char *s) {
+    syscall3(SYS_write, 1, (unsigned)s, strlen(s));
+}
+
+void putchar(char c) {
+    syscall3(SYS_write, 1, (unsigned)&c, 1);
+}
+
+void printu(unsigned n) {
+    if(n >= 10)
+        printu(n / 10);
+    putchar('0' + (n % 10));
+}
+
+int main() {
+    unsigned lines = 0;
+
+    char c;
+    while(1) {
+        int res = syscall3(SYS_read, 0, (unsigned)&c, 1);
+        if(res == 0)
+            break;
+        if(c == '\n')
+            lines++;
+    }
+
+    puts("Lines: ");
+    printu(lines);
+    putchar('\n');
+    return 0;
 }
